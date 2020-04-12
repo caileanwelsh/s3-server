5026468ee8ed166006c46845a38cdd238a7cb056
Cailean Welsh <cailean.twc@btinternet.com>
2020-04-12 22:31:22 +0100

    updated mongoose version

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
 
-const dbHost = 'mongodb://localhost/camselector';
+const dbHost = 'mongodb://localhost:27017/camselector';
 
 mongoose.connect(dbHost, {useNewUrlParser: true});
 
---
+++ schemas/CDM.js
@@ -0,0 +1,10 @@
+import { mongoose } from "mongoose";
+
+var Schema = mongoose.Schema;
+
+var CDM = new Schema({
+    CCSDS_CDM_VERS: Double,
+    CREATION_DATE:
+
+
+});
